1a0f5d2c035f2c78593b4198ea0fe7cb2810f377
Brandon Weaver <a7xncob@gmail.com>
2020-02-25 20:51:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const titleCased = () => {
   return tutorials.map(function(tutorial) {
     let words = tutorial.split(' ');
     return words.map(function(word) {
-      return word[0].toUpperCase();
+      return word.map(function(characters) {
+        return characters[0].toUpperCase();
+      });
     });
   });
 };
